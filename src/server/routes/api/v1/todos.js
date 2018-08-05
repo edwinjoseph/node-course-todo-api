9c3a8340ab49765296b7067f7f1eef49ed423893
Edwin Joseph <edwin@wonderbly.com>
2018-08-05 20:50:54 +0100

    Add GET /api/v1/todos/:id

--- src/server/routes/api/v1/todos.js
+++ src/server/routes/api/v1/todos.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb');
 const Todo = require('../../../models/todo');
 
 module.exports = app => {
@@ -22,6 +23,23 @@ module.exports = app => {
             })
             .catch(err => {
                 res.status(400).send(err);
+            })
+    });
+    app.get('/api/v1/todos/:id', (req, res) => {
+        const id = req.params.id;
+        if (!ObjectID.isValid(id)) {
+            return res.status(400).send({ error: { message: 'Invalid ID used.' }})
+        }
+
+        Todo.findById(id)
+            .then(todo => {
+                if (!todo) {
+                    return res.status(404).send({ error: { message: 'Todo not found.' }})
+                }
+                res.send({ todo });
+            })
+            .catch(err => {
+                res.status(500).send(err);
             })
     });
 };
