a1d2a616e1cb3b87a966e7b40c957720e3516653
Edwin Joseph <edwin@wonderbly.com>
2018-08-05 15:39:06 +0100

    Updtae delete file

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -4,22 +4,22 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
     .then(client => {
         console.log('Connected to MongoDB server');
         const db = client.db('TodoApp');
-        
-        db.collection('Users').find().toArray()
+
+        // db.collection('Users').deleteMany({ firstname: 'Edwin' })
+        //     .then(res => {
+        //         console.log(JSON.stringify(res, null, 2))
+        //     })
+        //     .catch(err => {
+        //         console.log('Unable to delete todos.', err);
+        //     });
+
+        db.collection('Users').findOneAndDelete({ _id: new ObjectID('5b670a298e50f5fd3ff44021') })
             .then(res => {
                 console.log(JSON.stringify(res, null, 2))
             })
             .catch(err => {
-                console.log('Unable to fetch todos.', err);
+                console.log('Unable to delete todos.', err);
             });
-
-        // db.collection('Users').insertOne({ firstname: 'Kezia', lastname: 'Joseph', age: 28, location: 'London' })
-        //     .then(res => {
-        //         console.log(res.ops[0]._id.getTimestamp())
-        //     })
-        //     .catch(err => {
-        //         console.log('Unable to insert user.', err);
-        //     });
 
         client.close();
     })
